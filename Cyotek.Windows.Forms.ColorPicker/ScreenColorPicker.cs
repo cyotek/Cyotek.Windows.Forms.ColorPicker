00331f1efb18ec013d7f79648ef09d7eaaf134b2
Thomas Epperson <thomas.epperson@gmail.com>
2018-11-08 16:00:47 -0500

    Add IWindowsFormsEditorService to ScreenColorPicker
    Add IWindowsFormsEditorService so that it can be easily used to select colors at design time.

--- Cyotek.Windows.Forms.ColorPicker/ScreenColorPicker.cs
+++ Cyotek.Windows.Forms.ColorPicker/ScreenColorPicker.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.Windows.Forms;
+using System.Windows.Forms.Design;
 
 namespace Cyotek.Windows.Forms
 {
@@ -53,6 +54,8 @@ namespace Cyotek.Windows.Forms
     private bool _showTextWithSnapshot;
 
     private int _zoom;
+    
+    private IWindowsFormsEditorService edSvc;
 
     #endregion
 
@@ -61,7 +64,7 @@ namespace Cyotek.Windows.Forms
     /// <summary>
     /// Initializes a new instance of the <see cref="ScreenColorPicker"/> class.
     /// </summary>
-    public ScreenColorPicker()
+    public ScreenColorPicker(IWindowsFormsEditorService edSvc)
     {
       this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.OptimizedDoubleBuffer, true);
       this.SetStyle(ControlStyles.Selectable | ControlStyles.StandardClick | ControlStyles.StandardDoubleClick, false);
@@ -72,6 +75,7 @@ namespace Cyotek.Windows.Forms
       this.TabIndex = 0;
       this.ShowGrid = true;
       this.GridColor = SystemColors.ControlDark;
+      this.edSvc = edSvc;
     }
 
     #endregion
@@ -460,6 +464,10 @@ namespace Cyotek.Windows.Forms
         this.Cursor = Cursors.Default;
         this.IsCapturing = false;
         this.Invalidate();
+        if (this.edSvc != null)
+        {
+            edSvc.CloseDropDown();
+        }
       }
     }
 
