0a1ea15c17d1e8c973ab5594cde987c7188f3f1e
Richard Moss <richard.moss@cyotek.com>
2021-11-14 08:53:31 +0000

    TEST: Added tests for new Navigate method

--- Cyotek.Windows.Forms.ColorPicker.Tests/ColorGridTests.cs
+++ Cyotek.Windows.Forms.ColorPicker.Tests/ColorGridTests.cs
@@ -22,7 +22,7 @@ namespace Cyotek.Windows.Forms.ColorPicker.Tests
   {
     #region Public Properties
 
-    public static IEnumerable<TestCaseData> NavigateTestCaseSource
+    public static IEnumerable<TestCaseData> NavigateOriginTestCaseSource
     {
       get
       {
@@ -46,13 +46,44 @@ namespace Cyotek.Windows.Forms.ColorPicker.Tests
       }
     }
 
+    public static IEnumerable<TestCaseData> NavigateTestCaseSource
+    {
+      get
+      {
+        yield return new TestCaseData(15, ColorGridNavigationMode.None, 15).SetName("{m}None");
+        yield return new TestCaseData(0, ColorGridNavigationMode.Previous, 0).SetName("{m}LeftBoundary");
+        yield return new TestCaseData(0, ColorGridNavigationMode.PreviousRow, 0).SetName("{m}UpBoundary");
+        yield return new TestCaseData(15, ColorGridNavigationMode.Previous, 14).SetName("{m}Left");
+        yield return new TestCaseData(15, ColorGridNavigationMode.Next, 16).SetName("{m}Right");
+        yield return new TestCaseData(15, ColorGridNavigationMode.PreviousRow, 5).SetName("{m}Up");
+        yield return new TestCaseData(15, ColorGridNavigationMode.NextRow, 25).SetName("{m}Down");
+        yield return new TestCaseData(69, ColorGridNavigationMode.NextRow, 78).SetName("{m}DownToPartialRow");
+        yield return new TestCaseData(88, ColorGridNavigationMode.PreviousRow, 78).SetName("{m}UpToPartialRow");
+        yield return new TestCaseData(0, ColorGridNavigationMode.Previous, 0).SetName("{m}LeftStart");
+        yield return new TestCaseData(0, ColorGridNavigationMode.Next, 1).SetName("{m}RightStart");
+        yield return new TestCaseData(0, ColorGridNavigationMode.PreviousRow, 0).SetName("{m}UpStart");
+        yield return new TestCaseData(0, ColorGridNavigationMode.NextRow, 10).SetName("{m}DownStart");
+        yield return new TestCaseData(97, ColorGridNavigationMode.Previous, 96).SetName("{m}LeftEnd");
+        yield return new TestCaseData(97, ColorGridNavigationMode.NextRow, 97).SetName("{m}RightEnd");
+        yield return new TestCaseData(97, ColorGridNavigationMode.PreviousRow, 88).SetName("{m}UpEnd");
+        yield return new TestCaseData(97, ColorGridNavigationMode.NextRow, 97).SetName("{m}DownEnd");
+        yield return new TestCaseData(69, ColorGridNavigationMode.NextRow, 78).SetName("{m}DownToStaggeredPrimary");
+        yield return new TestCaseData(70, ColorGridNavigationMode.NextRow, 79).SetName("{m}DownStartStaggeredPrimary");
+        yield return new TestCaseData(78, ColorGridNavigationMode.NextRow, 87).SetName("{m}DownEndStaggeredPrimary");
+        yield return new TestCaseData(78, ColorGridNavigationMode.PreviousRow, 68).SetName("{m}UpEndStaggeredPrimary");
+        yield return new TestCaseData(88, ColorGridNavigationMode.NextRow, 97).SetName("{m}DownToStaggeredSecondary");
+        yield return new TestCaseData(88, ColorGridNavigationMode.PreviousRow, 78).SetName("{m}UpToStaggeredPrimary");
+        yield return new TestCaseData(97, ColorGridNavigationMode.PreviousRow, 87).SetName("{m}UpEndStaggeredPrimary");
+      }
+    }
+
     #endregion Public Properties
 
     #region Public Methods
 
     [Test]
-    [TestCaseSource(nameof(NavigateTestCaseSource))]
-    public void NavigateTestCases(int index, int c, int r, NavigationOrigin origin, int expected)
+    [TestCaseSource(nameof(NavigateOriginTestCaseSource))]
+    public void NavigateOriginTestCases(int index, int c, int r, NavigationOrigin origin, int expected)
     {
       // arrange
       ColorGrid target;
@@ -63,6 +94,25 @@ namespace Cyotek.Windows.Forms.ColorPicker.Tests
 
       // act
       target.Navigate(c, r, origin);
+
+      // assert
+      actual = target.ColorIndex;
+      Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    [TestCaseSource(nameof(NavigateTestCaseSource))]
+    public void NavigateTestCases(int index, ColorGridNavigationMode mode, int expected)
+    {
+      // arrange
+      ColorGrid target;
+      int actual;
+
+      target = this.CreateGrid();
+      target.ColorIndex = index;
+
+      // act
+      target.Navigate(mode);
 
       // assert
       actual = target.ColorIndex;
